9818da0fbbbabfa7829084762524fdff7b74cead
Jerdon07 <jdlitaoen@gmail.com>
2026-06-11 21:35:23 +0800

    Feat: Add my-life section to get to know me

---
+++ lib/my-life.ts
@@ -0,0 +1,21 @@
+
+export const myLife = [
+        {
+            id: 1,
+            title: "My Programming Journey",
+            content: "My older brother introduced me to Web Development. I Started to learn HTML, CSS, and Basic JavaScript at the year 2022, right before college. University of the Cordilleras taught me more than just coding, it taught me the architectures and philosophies that isn't taken seriously in a general Programming Tutorials. The University is where I learned Object-Oriented Programming, Data Structures & Algorithms, System Analysis & Design, Communications in the Workplace, and other interesting things that I can use for my career in tech."
+        }, {
+            id: 2,
+            title: "Problems I Encountered",
+            content: "In early years of my college, I don't focus much on programming. Although I watched JavaScript episode by episode, started Python crash courses, introduced to PHP, I don't have clear vision how or where to start. The knowledge I gained during my college (OOP, DSA, etc...) seems to have no value. Aside from the programming problems, I also find it hard for me to focus on something especially if it's not interesting enough, on top of that I also tend to forget things pretty easily; This personal problems may be related to monotropism and low interoception."
+        }, {
+            id: 3,
+            title: "Introduction to Backend Development",
+            content: "In the last year of my college, I was introduced to Laravel framework. Although I can't say that I'm confident at PHP at the time, JavaScript syntax helped me understand the functionalities within. When I use the Laravel framework for capstone project is where I witnessed how it deeply utilizes Object-Oriented Programming and the Data Structures and Algorithms."
+        }, {
+            id: 4,
+            title: "Enhancing my Skills",
+            content: "If you have visited my GitHub profile, you know that I'm active Learning is a never ending process. I always try to find ways to sharpen my development skills, learn new software technologies, be active on tech updates."
+        }
+    ]
+
